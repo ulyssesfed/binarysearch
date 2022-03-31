f77ce5ac93b1a0ac7ad277b15ad6df6970761867
gf211097 <ulyfleen@gmail.com>
2022-03-31 12:34:40 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int[] arr = {2, 3, 4, 10, 40};
+        int key = 10;
+        int result = binarySearch(arr, key);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key < arr[mid]) {
+                high = mid - 1;
+            } else if (key > arr[mid]) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
